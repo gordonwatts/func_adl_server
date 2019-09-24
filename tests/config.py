85721073f8dfe285b42d25b7054732cdd445038f
Gordon Watts <gordon@gordonandpaula.net>
2019-09-23 22:12:27 -0700

    Update to fetch the node information correctly
    This will make this set of tests more robust.

--- tests/config.py
+++ tests/config.py
@@ -6,6 +6,9 @@ import logging
 import pytest
 import os
 from itertools import chain
+import tempfile
+import yaml
+from urllib.parse import urlparse
 
 
 def copy_file_to_container(container_name, file_uri, file_name):
@@ -69,27 +72,81 @@ def get_pod_status(name: str):
     return [{'name': p['metadata']['name'], 'status': all([s['ready'] for s in p['status']['containerStatuses']])} for p in data['items'] if p['metadata']['name'].startswith(name)]
 
 
+def _fetch_kube_node_ip():
+    '''
+    Get the head node so we can guess the internet address to use.
+    '''
+    result = subprocess.run(['kubectl', 'get', 'node', '-o', 'json'], stdout=subprocess.PIPE)
+    data = json.loads(result.stdout)
+    items = data['items']
+    if len(items) > 1:
+        raise BaseException('There is more than one node in this cluster - do not know how to figure out what is the internet address')
+    addresses = items[0]['status']['addresses']
+    internal_address = [a for a in addresses if a['type'] == 'InternalIP']
+    if len(internal_address) == 0:
+        raise BaseException('No internal ip address for us to use!')
+    return internal_address[0]['address']
+
+def _fetch_cluster_head_node():
+    '''
+    Get the head node of a cluster - we can use this to send and query things.
+    Works only on a single node cluster.
+    '''
+    result = subprocess.run(['kubectl', 'config', 'view'], stdout=subprocess.PIPE)
+    with tempfile.TemporaryFile() as t:
+        t.write(result.stdout)
+        t.seek(0)
+        data = yaml.safe_load(t)
+        if len(data['clusters']) != 1:
+            raise BaseException('Do not know how to deal with multiple clusters')
+        head_url = data['clusters'][0]['cluster']['server']
+        host = urlparse(head_url).hostname
+        return 'localhost' if host == 'kubernetes.docker.internal' else host
+
+
+def _yaml_contains(fname: str, top_level_name: str) -> bool:
+    '''
+    Returns true if the yaml file contains at top level a column name
+    '''
+    with open(fname, 'r') as i:
+        d = yaml.safe_load(i)
+        return top_level_name in d
+
+
 def start_helm_chart(chart_name: str, restart_if_running: bool = False, config_files=['tests/test-default-cluster.yaml']):
     '''
     Start the testing chart.
 
     Returns:
         chart-name      Name of the started chart.
+        IP-Address      Where to contact anything running in the new chart
     '''
+    ip_address = _fetch_cluster_head_node()
     if is_chart_running(chart_name) and not restart_if_running:
         logging.info(f'Decent chart with name {chart_name} already running. We can use it for testing.')
-        return chart_name
+        return (chart_name, ip_address)
 
     # Ok, make sure helm is clear of anything left over.
     stop_helm_chart(chart_name)
     logging.info(f'Starting chart {chart_name}.')
 
-    # Start the chart now that the system is clean.
-    cmd = ['helm', 'install', '--name', chart_name] + list(chain.from_iterable([['-f', f] for f in config_files])) + ['func-adl-server']
-    result = subprocess.run(cmd, stdout=subprocess.PIPE)
-    if result.returncode != 0:
-        stop_helm_chart(chart_name)
-        raise BaseException("Unable to start test helm chart")
+    # Write out a temp file config file that contains the proper setup for re-writing the IP address that
+    # comes back from the server.
+    with tempfile.TemporaryDirectory() as tempdir:
+        endpoint_path = os.path.join(tempdir, 'endpoint.yaml')
+        with open(endpoint_path, 'w') as o:
+            o.writelines([
+                'external_interface:\n',
+                f'  node_name: {ip_address}\n'
+            ])
+        all_config_files = config_files + [endpoint_path.replace('\\', '/')]
+
+        # Start the chart now that the system is clean.
+        cmd = ['helm', 'install', '--name', chart_name] + list(chain.from_iterable([['-f', f] for f in all_config_files])) + ['func-adl-server']
+        result = subprocess.run(cmd, stdout=subprocess.PIPE)
+        if result.returncode != 0:
+            stop_helm_chart(chart_name)
+            raise BaseException("Unable to start test helm chart")
 
     # Now, wait until it is up and running. The initial sleep is because if we don't, the containers
     # may not have status associated with them!
@@ -101,7 +158,7 @@ def start_helm_chart(chart_name: str, restart_if_running: bool = False, config_f
         is_ready = all(s['status'] for s in status)
         if is_ready:
             logging.info(f'All pods from chart {chart_name} are ready.')
-            return chart_name
+            return (chart_name, ip_address)
 
 
 if __name__ == '__main__':
@@ -114,9 +171,8 @@ def running_backend():
     'Configure a backend that is up and running. Will not restart if it is running'
     c_name = 'func-adl-testing-server'
 
-    if not is_chart_running(c_name):
-        start_helm_chart(c_name)
-    return "http://localhost:31000"
+    (_, ip_address) = start_helm_chart(c_name, restart_if_running=False)
+    return f"http://{ip_address}:31000"
 
 
 @pytest.fixture(scope='session')
@@ -124,23 +180,25 @@ def restarted_backend():
     'Configure a backend that gets restarted if it is currently running.'
     c_name = 'func-adl-testing-server'
 
-    start_helm_chart(c_name, restart_if_running=True)
-    return "http://localhost:31000"
+    (_, ip_address) = start_helm_chart(c_name, restart_if_running=True)
+    return f'http://{ip_address}:31000'
 
 
 @pytest.fixture
 def single_use_auth_cluster_restart():
     'Configure a backend that will create an authenticated cluster, making sure it is clean'
     c_name = 'func-adl-testing-auth-server'
-    start_helm_chart(c_name, restart_if_running=True, config_files=['../func-adl-rucio-cert.yaml', 'tests/test-auth-cluster.yaml'])
-    yield "http://localhost:31005"
+    (_, ip_address) = start_helm_chart(c_name, restart_if_running=True, config_files=['../func-adl-rucio-cert.yaml', 'tests/test-auth-cluster.yaml'])
+    yield f"http://{ip_address}:31005"
+
 
 @pytest.fixture
 def single_use_auth_cluster():
     'Configure a backend that will create an authenticated cluster - re-use one that is already there'
     c_name = 'func-adl-testing-auth-server'
-    start_helm_chart(c_name, restart_if_running=False, config_files=['../func-adl-rucio-cert.yaml', 'tests/test-auth-cluster.yaml'])
-    yield "http://localhost:31005"
+    (_, ip_address) = start_helm_chart(c_name, restart_if_running=False, config_files=['../func-adl-rucio-cert.yaml', 'tests/test-auth-cluster.yaml'])
+    yield f"http://{ip_address}:31005"
+
 
 certs_available = pytest.mark.skipif(
     not os.path.exists('../func-adl-rucio-cert.yaml'),
